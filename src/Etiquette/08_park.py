b820f112aae919a3780bc0b669e30bce27037fc0
daaa02 <dyk98498@gmail.com>
2022-12-07 22:00:49 +0900

    Update_221207

--- src/Etiquette/08_park.py
+++ src/Etiquette/08_park.py
@@ -6,10 +6,10 @@ import os, sys
 import re
 import random
 
-sys.path.append('/home/kiro/workspace/Conversation_Scenarios/data')
-from conversation_manage import ConversationManage, WordManage
-from speech_to_text import speech_to_text
-from text_to_speech import TextToSpeech, text_to_speech
+sys.path.append('/home/kiro/workspace/Conversation_Scenarios/')
+from data.conversation_manage import ConversationManage, WordManage
+from data.speech_to_text import speech_to_text
+from data.text_to_speech import TextToSpeech, text_to_speech
 
 cm = ConversationManage()
 wm = WordManage()
