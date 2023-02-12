fe0ab44412141b2e4fb46c425c0c571246b412af
daaa02 <dyk98498@gmail.com>
2023-02-12 18:00:49 +0900

    Update 15_queenbee.py

--- src/Fairytale/15_queenbee.py
+++ src/Fairytale/15_queenbee.py
@@ -30,20 +30,20 @@ class Fairytale():
         answer = cm.responses_proc(re_bhv="do_joy_A", re_q=f"{wm.word(self.user_name, 0)}는 어떤 장면이 재미있었니?",
                                    neu_bhv="do_agree", neu=f"그럴 수 있지~")
         
-        cm.tts(bhv="question_S", string=f"개미들이 찾아준 천 개의 진주로 무엇을 만들었을까?")
+        cm.tts(bhv="do_question_S", string=f"개미들이 찾아준 천 개의 진주로 무엇을 만들었을까?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"물건을 잃어버렸을 때는 어떻게 찾아야할까?", 
                                    neu_bhv="do_agree", neu=f"괜찮아~ 대답하기 어려울 수 있어~")
 
-        cm.tts(bhv="question_S", string=f"만약 막내 왕자도 개미를 괴롭혔다면 개미들은 막내를 도와줬을까?")
+        cm.tts(bhv="do_question_S", string=f"만약 막내 왕자도 개미를 괴롭혔다면 개미들은 막내를 도와줬을까?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 물건을 물 속에 빠뜨린 적 있니?", 
                                    neu_bhv="do_agree", neu=f"괜찮아~ 생각나지 않을 수 있어~")   
         
-        cm.tts(bhv="question_S", string=f"돌이 된 형들은 무슨 생각을 했을까?")
+        cm.tts(bhv="do_question_S", string=f"돌이 된 형들은 무슨 생각을 했을까?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"돌이 된 형들은 무슨 생각을 했을까?", 
                                    neu_bhv="do_agree", neu=f"모를 수 있어~")  
 
         # 2. 등장인물 공감 대화        
-        cm.tts(bhv="question_S", string=f"난쟁이에게 어려운 임무를 받았을 때 막내 왕자는 실패할까봐 걱정했겠지?")
+        cm.tts(bhv="do_question_S", string=f"난쟁이에게 어려운 임무를 받았을 때 막내 왕자는 실패할까봐 걱정했겠지?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"물건을 잃어버렸을 때는 어떻게 찾아야할까?", 
                                    pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 걱정이 되는 일이 있었다면 말해 줄래?", 
                                    neu_bhv="do_agree", neu=f"몰라도 괜찮아~", 
@@ -53,7 +53,7 @@ class Fairytale():
             answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 최근에 걱정이 되는 일이 있었다면 말해 줄래?",
                                        act_bhv="do_sad", act=f"걱정됐겠구나!")
             
-        cm.tts(bhv="question_S", string=f"개미랑 여왕벌이 도와주었을 때 막내 왕자는 감동 받았을까?")
+        cm.tts(bhv="do_question_S", string=f"개미랑 여왕벌이 도와주었을 때 막내 왕자는 감동 받았을까?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"개미랑 여왕벌이 도와주었을 때 막내 왕자는 감동 받았을까?", 
                                    pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 감동 받은 적이 있다면 말해 줄래?", 
                                    neu_bhv="do_agree", neu=f"괜찮아~ 대답하기 어려울 수 있어~", 
