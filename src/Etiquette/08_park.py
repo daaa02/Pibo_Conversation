ee52f824399094ca0e56608c2db4c87d64fa51fc
daaa02 <dyk98498@gmail.com>
2023-02-12 16:18:38 +0900

    Update 08_park.py

--- src/Etiquette/08_park.py
+++ src/Etiquette/08_park.py
@@ -27,9 +27,9 @@ class Etiquette():
 
         # 2.1 카드 대화
         cm.tts(bhv="do_question_L", string="이 카드의 어린이는 무엇을 잘못했을까?")
-        answer = cm.responses_proc(re_bhv="do_question_L", re_bhv="", re_q="이 카드의 어린이는 무엇을 잘못했을까?",
+        answer = cm.responses_proc(re_bhv="do_question_L", re_q="이 카드의 어린이는 무엇을 잘못했을까?",
                                    neg_bhv="do_suggestion_S", neg="같이 다시 한번 볼까?",
-                                   neu_bhv="do_suggestion_S", neu_bhv="", neu="같이 다시 한번 볼까?")             
+                                   neu_bhv="do_suggestion_S", neu="같이 다시 한번 볼까?")             
         
         if answer[0] == "action":            
             
@@ -45,7 +45,7 @@ class Etiquette():
             else:
                 print(self.ox)
                 cm.tts(bhv="do_suggestion_S", string="또 무엇을 잘못했을까?")
-                answer = cm.responses_proc(re_bhv="do_suggestion_S", re_bhv="", re_q="또 무엇을 잘못했을까?")
+                answer = cm.responses_proc(re_bhv="do_suggestion_S", re_q="또 무엇을 잘못했을까?")
                 
                 if answer[0] == "action":        
                                 
@@ -62,12 +62,12 @@ class Etiquette():
                         print(self.ox)
                         cm.tts(bhv="do_suggestion_S", string="같이 다시 한번 볼까?")
 
-        cm.tts(bhv="", string="이 카드의 어린이는 공원의 잔디를 밟고 꽃을 꺾었어.")
+        cm.tts(bhv="do_question_S", string="이 카드의 어린이는 공원의 잔디를 밟고 꽃을 꺾었어.")
         
         # 2.2 경험 질문
-        cm.tts(f"{wm.word(self.user_name, type=0)}는 공원에 가면 무엇을 하니?")
-        answer = cm.responses_proc(re_bhv="", re_q=f"{wm.word(self.user_name, type=0)}는 공원에 가면 무엇을 하니?",
-                                   neg = "괜찮아, 기억이 안 날 수 있어.")
+        cm.tts(bhv="do_question_S", string=f"{wm.word(self.user_name, type=0)}는 공원에 가면 무엇을 하니?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, type=0)}는 공원에 가면 무엇을 하니?",
+                                   neu_bhv="do_agree", neu="괜찮아, 기억이 안 날 수 있어.")
         
         cm.tts(bhv="do_question_S", string="공원에 가면 사람들이 무엇을 하고 있니?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q="공원에 가면 사람들이 무엇을 하고 있니?",
@@ -76,7 +76,7 @@ class Etiquette():
         
         cm.tts(bhv="do_question_S", string="공원의 잔디와 꽃을 망가뜨리면 어떤 일이 일어날까?")
         answer = cm.responses_proc(re_bhv="do_question_S", re_q="공원의 잔디와 꽃을 망가뜨리면 어떤 일이 일어날까?",
-                                   pos="예쁜 공원이 망가지고 사람들은 쉴 수 없겠지?",
+                                   pos_bhv="do_agree", pos="예쁜 공원이 망가지고 사람들은 쉴 수 없겠지?",
                                    act_bhv="do_agree", act="예쁜 공원이 망가지고 사람들은 쉴 수 없겠지?",
                                    neu_bhv="do_agree", neu="괜찮아 모를 수도 있어~ 예쁜 공원이 망가지고 사람들은 쉴 수 없겠지?")
         
