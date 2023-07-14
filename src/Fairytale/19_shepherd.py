77d188ec4fb371d8132f2ada57c14cc78c12e227
daaa02 <dyk98498@gmail.com>
2023-07-14 16:45:49 +0900

    Update_230714

--- src/Fairytale/19_shepherd.py
+++ src/Fairytale/19_shepherd.py
@@ -47,90 +47,90 @@ class Fairytale():
         
     def Shepherd(self):
         
-        # # 0. 동화 읽어주기
-        # pibo = cm.tts(bhv="do_breath1", string=f"내가 재미있는 이야기를 들려 줄게. 동화 제목은 {wm.word(self.story_name, 0)}야.")
-        # f = open('/home/pi/Pibo_Conversation/src/Fairytale/data/19_shepherd_story.txt', 'r')
-        # lines = f.readlines()
-        # # print(lines)
+        # 0. 동화 읽어주기
+        pibo = cm.tts(bhv="do_breath1", string=f"내가 재미있는 이야기를 들려 줄게. 동화 제목은 {wm.word(self.story_name, 0)}야.")
+        f = open('/home/pi/Pibo_Conversation/src/Fairytale/data/19_shepherd_story.txt', 'r')
+        lines = f.readlines()
+        # print(lines)
         
-        # for i in range(0, len(lines), 2):
-        #     try:
-        #         text_to_speech(voice="nara", text=f"{lines[i]}")
-        #         text_to_speech(voice="ndain", text=f"{lines[i+1]}")
-        #     except IndexError as e:
-        #         break
+        for i in range(0, len(lines), 2):
+            try:
+                text_to_speech(voice="nara", text=f"{lines[i]}")
+                text_to_speech(voice="ndain", text=f"{lines[i+1]}")
+            except IndexError as e:
+                break
         
-        # # 1. 동화 줄거리 대화        
-        # pibo = cm.tts(bhv="do_joy_A", string=f"정말 재미있는 이야기였어! {wm.word(self.user_name, 0)}는 어떤 장면이 재미있었니?")
-        # answer = cm.responses_proc(re_bhv="do_joy_A", re_q=f"{wm.word(self.user_name, 0)}는 어떤 장면이 재미있었니?",
-        #                            neu_bhv="do_compliment_S", neu=f"그럴 수 있지.")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        # 1. 동화 줄거리 대화        
+        pibo = cm.tts(bhv="do_joy_A", string=f"정말 재미있는 이야기였어! {wm.word(self.user_name, 0)}는 어떤 장면이 재미있었니?")
+        answer = cm.responses_proc(re_bhv="do_joy_A", re_q=f"{wm.word(self.user_name, 0)}는 어떤 장면이 재미있었니?",
+                                   neu_bhv="do_compliment_S", neu=f"그럴 수 있지.")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
         
-        # pibo = cm.tts(bhv="do_question_S", string=f"양치기 소년은 양들을 돌볼 때 힘들겠지?")
-        # answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년은 양들을 돌볼 때 힘들겠지?")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        pibo = cm.tts(bhv="do_question_S", string=f"양치기 소년은 양들을 돌볼 때 힘들겠지?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년은 양들을 돌볼 때 힘들겠지?")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
         
-        # pibo = cm.tts(bhv="do_question_S", string=f"양치기 소년이 돌보는 양은 몇 마리 일지 상상해볼까?")
-        # answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년이 돌보는 양은 몇 마리 일지 상상해볼까?",
-        #                            neu_bhv="do_compliment_S", neu=f"몰라도 괜찮아.")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        pibo = cm.tts(bhv="do_question_S", string=f"양치기 소년이 돌보는 양은 몇 마리 일지 상상해볼까?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년이 돌보는 양은 몇 마리 일지 상상해볼까?",
+                                   neu_bhv="do_compliment_S", neu=f"몰라도 괜찮아.")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
         
-        # pibo = cm.tts(bhv="do_question_S", string=f"그 많은 양들은 풀을 얼마나 먹을까?")
-        # answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"그 많은 양들은 풀을 얼마나 먹을까?")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        pibo = cm.tts(bhv="do_question_S", string=f"그 많은 양들은 풀을 얼마나 먹을까?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"그 많은 양들은 풀을 얼마나 먹을까?")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
 
-        # # 2. 등장인물 공감 대화        
-        # pibo = cm.tts(bhv="question_S", string=f"양치기 소년에게 속은 마을 사람들은 화가 났을까?")
-        # answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년에게 속은 마을 사람들은 화가 났을까?", 
-        #                            pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?",
-        #                            act_bhv="do_question_S", act=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        # 2. 등장인물 공감 대화        
+        pibo = cm.tts(bhv="question_S", string=f"양치기 소년에게 속은 마을 사람들은 화가 났을까?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"양치기 소년에게 속은 마을 사람들은 화가 났을까?", 
+                                   pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?",
+                                   act_bhv="do_question_S", act=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
 
-        # if answer[0][0] == "positive" or answer[0][0] == "action":
-        #     answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?",
-        #                                neu_bhv="do_compliment_S", neu=f"괜찮아. 생각 나지 않을 수 있어.", 
-        #                                act_bhv="do_sad", act=f"정말 화가 났었겠구나!")
-        #     cwc.writerow(['pibo', pibo])
-        #     cwc.writerow(['user', answer[0][1], answer[1]])
-        #     self.reject.append(answer[1]) 
+        if answer[0][0] == "positive" or answer[0][0] == "action":
+            answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 최근에 속아서 화가 난 적이 있다면 말해 줄 수 있니?",
+                                       neu_bhv="do_compliment_S", neu=f"괜찮아. 생각 나지 않을 수 있어.", 
+                                       act_bhv="do_sad", act=f"정말 화가 났었겠구나!")
+            cwc.writerow(['pibo', pibo])
+            cwc.writerow(['user', answer[0][1], answer[1]])
+            self.reject.append(answer[1]) 
             
-        # pibo = cm.tts(bhv="question_S", string=f"거짓말을 한 양치기 소년은 후회했을까?")
-        # answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"거짓말을 한 양치기 소년은 후회했을까?", 
-        #                            pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?", 
-        #                            neu_bhv="do_compliment_S", neu=f"괜찮아. 생각 나지 않을 수 있어.", 
-        #                            act_bhv="do_question_S", act=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        pibo = cm.tts(bhv="question_S", string=f"거짓말을 한 양치기 소년은 후회했을까?")
+        answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"거짓말을 한 양치기 소년은 후회했을까?", 
+                                   pos_bhv="do_question_S", pos=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?", 
+                                   neu_bhv="do_compliment_S", neu=f"괜찮아. 생각 나지 않을 수 있어.", 
+                                   act_bhv="do_question_S", act=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
 
-        # if answer[0][0] == "positive" or answer[0][0] == "action":
-        #     answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?",
-        #                                pos_bhv="do_sad", pos=f"속상했겠다.",
-        #                                act_bhv="do_sad", act=f"속상했겠다.")        
-        #     cwc.writerow(['pibo', pibo])
-        #     cwc.writerow(['user', answer[0][1], answer[1]])
-        #     self.reject.append(answer[1]) 
+        if answer[0][0] == "positive" or answer[0][0] == "action":
+            answer = cm.responses_proc(re_bhv="do_question_S", re_q=f"{wm.word(self.user_name, 0)}도 최근에 후회 한 적이 있다면 말해줄래?",
+                                       pos_bhv="do_sad", pos=f"속상했겠다.",
+                                       act_bhv="do_sad", act=f"속상했겠다.")        
+            cwc.writerow(['pibo', pibo])
+            cwc.writerow(['user', answer[0][1], answer[1]])
+            self.reject.append(answer[1]) 
             
-        # # 3. 마무리 대화    
-        # pibo = cm.tts(bhv="do_question_L", string=f"자꾸 거짓말을 하는 양치기 소년에게 뭐라고 해주고 싶니?")
-        # answer = cm.responses_proc(re_bhv="do_question_L", re_q=f"자꾸 거짓말을 하는 양치기 소년에게 뭐라고 해주고 싶니?",  
-        #                            pos_bhv="do_compliment_S", pos=f"그렇구나!",
-        #                            neu_bhv="do_compliment_S", neu=f"괜찮아. 모를 수 있지. ",
-        #                            neg_bhv="do_compliment_S", neg=f"그렇구나!",
-        #                            act_bhv="do_compliment_S", act=f"그렇구나!")
-        # cwc.writerow(['pibo', pibo])
-        # cwc.writerow(['user', answer[0][1], answer[1]])
-        # self.reject.append(answer[1]) 
+        # 3. 마무리 대화    
+        pibo = cm.tts(bhv="do_question_L", string=f"자꾸 거짓말을 하는 양치기 소년에게 뭐라고 해주고 싶니?")
+        answer = cm.responses_proc(re_bhv="do_question_L", re_q=f"자꾸 거짓말을 하는 양치기 소년에게 뭐라고 해주고 싶니?",  
+                                   pos_bhv="do_compliment_S", pos=f"그렇구나!",
+                                   neu_bhv="do_compliment_S", neu=f"괜찮아. 모를 수 있지. ",
+                                   neg_bhv="do_compliment_S", neg=f"그렇구나!",
+                                   act_bhv="do_compliment_S", act=f"그렇구나!")
+        cwc.writerow(['pibo', pibo])
+        cwc.writerow(['user', answer[0][1], answer[1]])
+        self.reject.append(answer[1]) 
         
         pibo = cm.tts(bhv="do_joy_B", string=f"오늘 동화 재미있었지? 다음에 또 재미있는 동화를 들려줄게.")
         
