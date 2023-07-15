8d80fb92d4f467667aec939e0e41562842194a32
daaa02 <dyk98498@gmail.com>
2023-07-15 17:01:14 +0900

    Update_230715

--- src/schedule_run.py
+++ src/schedule_run.py
@@ -56,7 +56,7 @@ class RunSchedule():
             #        4:'Pibo_Play/src/Soc/soc_6.py', 5:'Pibo_Conversation/src/Etiquette/03_cough.py', 
             #        6:'Pibo_Play/src/Com/com_4.py', 7:'Pibo_Conversation/src/Solution/01_badword.py'}            
             fix = {0:'Pibo_Conversation/src/Fairytale/19_shepherd.py', 1:'Pibo_Conversation/src/Roleplay/02_strong.py',
-                   2:'Pibo_Conversation/src/Solution/01_badword.py', 3:'Pibo_Play/src/Soc/soc_6.py',
+                   2:'Pibo_Conversation/src/Solution/01_badword.py', 3:'Pibo_Play/src/Soc/soc_2.py',
                    4: 'Pibo_Play/src/Cog/cog_1.py', 5:'Pibo_Conversation/src/Etiquette/03_cough.py', 
                    6:'Pibo_Play/src/Com/com_4.py', 7:'Pibo_Play/src/Mus/mus_11.py'}   
             self.act = fix.get(self.completion)
